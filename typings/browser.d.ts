c02fddff9571e96068d3bfecd9f69167e7df3673
jkuri <jkuri88@gmail.com>
2016-02-23 20:40:31 +0100

    added typings to .gitignore

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/angular-protractor/angular-protractor.d.ts" />
-/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="browser/ambient/jasmine/jasmine.d.ts" />
-/// <reference path="browser/ambient/selenium-webdriver/selenium-webdriver.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/angular-protractor/angular-protractor.d.ts" />
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="main/ambient/jasmine/jasmine.d.ts" />
-/// <reference path="main/ambient/selenium-webdriver/selenium-webdriver.d.ts" />
